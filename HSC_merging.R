f8508b2b752cbef7167840e8fc9693bec2edb568
Rohitt624 <rthalla@luc.edu>
2024-05-21 15:19:19 -0500

    Add files via upload

---
+++ HSC_merging.R
@@ -0,0 +1,53 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+library(ggplot2)
+options(future.globals.maxSize = 2000 * 1024^2)
+
+setwd("C:/Users/rohit/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq")
+# Load the HSC dataset
+HSC.data <- Read10X(data.dir = "C:/Users/rohit/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/HSC/HSC_P7")
+
+# Pre-processing
+HSC <- CreateSeuratObject(counts = HSC.data, project = "HSC", min.cells = 3, min.features = 200)
+rm(HSC.data)
+HSC_split <- SplitObject(HSC, split.by = "orig.ident")
+rm(HSC)
+HSC_merged <- merge(x = HSC_split$HSC.E16.5, y = list(HSC_split$HSC.P7, HSC_split$HSC.P14, HSC_split$HSC.Adult))
+rm(HSC_split)
+HSC_merged <- NormalizeData(object = HSC_merged)
+HSC_merged <- FindVariableFeatures(object = HSC_merged)
+HSC_merged <- ScaleData(object = HSC_merged)
+HSC_merged <- RunPCA(object =HSC_merged)
+HSC_merged <- IntegrateLayers(object =HSC_merged, method = RPCAIntegration, orig.reduction = "pca", new.reduction = "integrated.rpca",
+                              verbose = FALSE)
+HSC_joined <- JoinLayers(HSC_merged)
+rm(HSC_merged)
+
+#Cluster
+HSC_joined <- FindNeighbors(object =HSC_joined, dims = 1:30)
+HSC_joined <- FindClusters(object =HSC_joined)
+HSC_joined <- RunUMAP(object =HSC_joined, dims = 1:30)
+DimPlot(object =HSC_joined, reduction = "umap")
+DimPlot(HSC_joined, group.by = c("orig.ident"))
+DimPlot(HSC_joined, split.by = c("orig.ident"))
+
+#identify markers in all clusters
+HSC.markers <- FindAllMarkers(HSC_joined)
+write.csv(HSC.markers, "allHSCmarkers.csv")
+DoHeatmap(HSC, features = top10$gene) + NoLegend()
+
+#plots
+VlnPlot(HSC_joined, features = c("Cd27", "Cd34", "Esam", "Sell", "Ctnnal1"))
+VlnPlot(HSC_joined, features = c("Cd34"))
+FeaturePlot(HSC_joined, features = c("Sell"))
+
+#Subset
+HSC_joined[["percent.Ctnnal1"]] <- PercentageFeatureSet(object = HSC_joined, features = c("Ctnnal1"))
+VlnPlot(HSC_joined, features = c("percent.Ctnnal1"))
+quartiles <- quantile(FetchData(HSC_joined, vars = c("Ctnnal1")), probs = c(0.25, 0.5, 0.55, 0.75), na.rm = TRUE)
+print(quartiles)
+
+HSC_Ctnnal1 <- subset(HSC_joined, subset = Ctnnal1 > 0)
+HSC_noCtnnal1 <- subset(HSC_joined, subset = WhichCells(HSC_joined, expression = Ctnnal1 == 0))
+cells0 <- WhichCells(HSC_joined, expression = Ctnnal1 == 0)
