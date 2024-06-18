0077ba4ace6b739aae597b4c183004c5d3d9145c
Rohitt624 <rthalla@luc.edu>
2024-06-18 12:31:39 -0500

    Update updated seurat with clustify.R

--- updated seurat with clustify.R
+++ updated seurat with clustify.R
@@ -7,10 +7,10 @@ library(patchwork)
 library(AUCell)
 library(GSEABase)
 options(future.globals.maxSize = 2000 * 1024^2)
-setwd("C:/Users/rohit/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/GSM4645164_Adult_WT")
+setwd("C:/Users/rthalla/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/GSM4645164_Adult_WT")
 
-#Load and pre-process scRNAseq data
-rnaseq.data <- Read10X(data.dir="C:/Users/rohit/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/GSM4645164_Adult_WT")
+#Load and pre-process scRNAseq data --------------------
+rnaseq.data <- Read10X(data.dir="C:/Users/rthalla/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/GSM4645164_Adult_WT")
 seurat.object <- CreateSeuratObject(counts = rnaseq.data, project = "MPC")
 rm(rnaseq.data)
 seurat.object <- NormalizeData(object = seurat.object)
@@ -23,11 +23,14 @@ seurat.object <- FindNeighbors(object =seurat.object, dims = 1:30)
 seurat.object <- FindClusters(object =seurat.object)
 seurat.object <- RunUMAP(object =seurat.object, dims = 1:30)
 DimPlot(object =seurat.object, reduction = "umap")
+umap <- DimPlot(object =seurat.object, reduction = "umap")
 
+#Visualization ----------------------------------------------
 FeaturePlot(seurat.object, features = c("Cd34", "Fcgr3"))
 VlnPlot(seurat.object, features = c("Cd27", "Cd34", "Sell")) 
 RidgePlot(seurat.object, features = c("Itga2b", "Eng")) 
 
+#Further processing -------------------------------------------
 #Subset one population
 group1 <- subset(seurat.object, subset = idents %in% c("CMP"))
 group1 <- NormalizeData(object = group1)
@@ -78,43 +81,27 @@ s2$PM <- ifelse(s2$Cd27_highvslow == 'Cd27Low' & s2$Cd34_highvslow == 'Cd34Low',
 DimPlot(s2, group.by = 'PM')
 table(s2$PM)
 
-#clustifyr 
-ref <- read.csv("C:/Users/rohit/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/MPC Bulk Seq Data/MPC_Counts_Ref.csv")
+#clustifyr -------------------------
+ref <- read.csv("C:/Users/rthalla/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/MPC Bulk Seq Data/MPC_Counts_Ref.csv")
 ref <- ref[, -1] #only necessary if bulk seq data has ensembl id before gene names
 ref2 <- ref[, -1]
 table(duplicated(ref2$Gene.name)) #check for duplicate gene names
 rownames(ref2) <- make.names(ref$Gene.name, unique = TRUE) #forces gene names to become unique
 #Calculate (it will automatically add a metadata column with the label)
 res <- clustify(
-  input = s2,
+  input = seurat.object,
   ref_mat = ref2,
   cluster_col = "seurat_clusters",
   obj_out = TRUE
 )
-
-clusters.markers <- FindAllMarkers(s2)
-write.csv(clusters.markers, "allmarkers.csv")
-
 DimPlot(res, group.by = c("type"))
 
-Idents(s2) <- "PM"
-s2.markers <- FindAllMarkers(s2)
-write.csv(s2.markers, "alls2markers.csv")
-top_genes <- FindAllMarkers(s2, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25) %>%
-  group_by("PM") %>%
-  top_n(n = 10, wt = avg_logFC)
 
 # Generate the heatmap
 DoHeatmap(s2, features = top_genes$gene, slot = )
 DimPlot(group1, group.by = c("Cd34_highvslow"))
 
-#AUCell
-rnaseq.data <- Read10X(data.dir="C:/Users/rohit/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/GSM4645164_Adult_WT")
-exprMatrix <- rnaseq.data
-rm(rnaseq.data)
-exprMatrix <- exprMatrix[unique(rownames(exprMatrix)),]
-dim(exprMatrix)
-exprMatrix[1:5,1:4]
+#AUCell -----------------------------------
 gHSC <- c("Procr", "Pdzk1ip1", "Ltb", "Mllt3", "Ifitm1", "Gimap1", "Gimap6", "Limd2", "Trim47", "Neil2", "Vwf",
           "Pde1b", "Neo1", "Sqrdl", "Sult1a1", "Cd82", "Ramp2", "Ubl3", "Ly6a", "Cdkn1c", "Fgfr3", "Cldn10", "Ptpn14", 
           "Mettl7a1", "Smtnl1", "Ctsf", "Gstm1", "Sox18", "Fads3")
@@ -122,35 +109,44 @@ gCMP <- c("Mpo", "Lars2", "Gm23935", "Rpl18a", "Gpx1", "Gm42418", "Gm26917", "Ee
           "Ftl1", "Gapdh", "Eif5a", "Prtn3", "Rpl13", "Rps18", "Rps3", "Elane", "Ly6e", "Rps14", "Rps19", "H2-D1", 
           "Rack1")
 HSCSets <- GeneSet(gHSC, setName="HSC")
-hsc.auc <- AUCell_run(exprMatrix, HSCSets)
-set.seed(333)
-par(mfrow=c(3,3))
-cells_assignment <- AUCell_exploreThresholds(hsc.auc, plotHist=TRUE, assign=TRUE) #see threshold as graph
-cells_assignment$HSC$aucThr$thresholds #see threshold as table
-HSCAssigned <- cells_assignment$HSC$assignment
-length(HSCAssigned)
+CMPSets <- GeneSet(gCMP, setName="CMP")
+geneSets <- GeneSetCollection(c(HSCSets, CMPSets))
 
-#to manually set a new threshold
-#geneSetName <- rownames(hsc.auc)[grep("HSC", rownames(hsc.auc))]
-#AUCell_plotHist(hsc.auc[geneSetName,], aucThr=0.25)
-#abline(v=0.25)
+# Convert Seurat object to SingleCellExperiment
+sce_complete <- as.SingleCellExperiment(seurat.object)
+
+# Calculate cell rankings
+cell_rankings <- assay(sce_complete, "counts") %>% AUCell_buildRankings()
+
+# Calculate signature scores
+signature_scores_sce_complete <- AUCell_calcAUC(geneSets, cell_rankings)
+
+# Add signature scores to colData
+colData(sce_complete) <- cbind(colData(sce_complete), t(signature_scores_sce_complete@assays@data$AUC)[colnames(sce_complete),])
+
+# Normalize counts
+sce_complete <- logNormCounts(sce_complete)
+
+# Get top HVGs
+hvg_complete <- getTopHVGs(dec_complete, n = 2000)
+
+# Run PCA on HVGs
+sce_complete <- runPCA(sce_complete, subset_row = hvg_complete)
+
+# Run UMAP on PCA
+sce_complete <- runUMAP(sce_complete, dimred="PCA", verbose = T, min_dist = 0.2, n_neighbors = 30, name= "Uncorrected_UMAP")
+
+# Plot UMAP
+plotReducedDim(sce_complete, dimred = "UMAP", colour_by = "HSC") 
+plotReducedDim(sce_complete, dimred = "UMAP", colour_by = "CMP")
 
 
-cellsAssigned <- lapply(cells_assignment, function(x) x$assignment)
-assignmentTable <- reshape2::melt(cellsAssigned, value.name="cell")
-colnames(assignmentTable)[2] <- "geneSet"
-head(assignmentTable)
 
-seurat.object[["HSC"]] <- HSCAssigned
-FeaturePlot(seurat.object, features = "HSC")
-
-
-
-#Applying annotation
+#Applying manual annotation ----------------
 Idents(seurat.object) <- "seurat_clusters" #make sure the clusters are the identity metadata column
 idents <- Idents(seurat.object)
 #Rename clusters
-cluster.ids <- c("HSC/MPP/LMPP", "CMP", "GP", "E-MEP", "GMP", "E-MEP", "MEP", "MK-MEP", "GMP", "MOP", "E-MEP", "MPP")
+cluster.ids <- c("HSC/MPP/LMPP", "CMP", "GP", "E-MEP 1", "GMP 1", "E-MEP 2", "MEP", "MK-MEP", "GMP 2", "MOP", "E-MEP 3", "MPP")
 names(cluster.ids) <- levels(seurat.object)
 seurat.object <- RenameIdents(seurat.object, cluster.ids)
 DimPlot(object =seurat.object, reduction = "umap")
