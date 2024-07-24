714694a498d7ecc7719f98b8d098344e9315cb21
Rohitt624 <rthalla@luc.edu>
2024-07-24 15:41:19 -0500

    Create gse81682.R

---
+++ gse81682.R
@@ -0,0 +1,38 @@
+library(SingleCellExperiment)
+library(dplyr)
+library(Seurat)
+library(ggplot2)
+library(cowplot)
+library(patchwork)
+options(future.globals.maxSize = 2000 * 1024^2)
+setwd("C:/Users/rthalla/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/GSE81682_HTSeq_counts.txt")
+
+
+counts <- read.table("C:/Users/rthalla/OneDrive - Loyola University Chicago/Zhang Lab/RNASeq/GSE81682_HTSeq_counts.txt/HTSeq_counts.txt", header=TRUE, row.names = 1)
+
+seu <- CreateSeuratObject(counts, project = "test", assay = "RNA", min.cells = 0, min.features = 0)
+seu <- NormalizeData(object = seu)
+#Processing
+seu <- FindVariableFeatures(object = seu)
+seu <- ScaleData(object = seu)
+seu <- RunPCA(object =seu)
+#Cluster
+seu <- FindNeighbors(object =seu, dims = 1:30)
+seu <- FindClusters(object =seu)
+seu <- RunUMAP(object =seu, dims = 1:30)
+DimPlot(object =seu, reduction = "umap")
+rm(counts)
+
+VlnPlot(seu, features = c("ENSMUSG00000005672", "ENSMUSG00000075602"))
+FeaturePlot(seu, features = c("ENSMUSG00000027611"))
+mark <- FindAllMarkers(seu)
+write.csv(mark, "markers.csv")
+
+#Applying manual annotation ----------------
+Idents(seu) <- "seurat_clusters" #make sure the clusters are the identity metadata column
+idents <- Idents(seu)
+#Rename clusters
+cluster.ids <- c("HSC/MPP/LMPP", "CMP", "GP", "CFU-E", "GMP", "CFU-E", "MEP", "MkP", "GMP", "Mast Cell Progenitor")
+names(cluster.ids) <- levels(seu)
+seu <- RenameIdents(seu, cluster.ids)
+DimPlot(object =seu, reduction = "umap")
