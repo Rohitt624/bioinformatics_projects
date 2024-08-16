a20541f35eacdcb03cc7e2c9e35fafa85abb18d0
Rohitt624 <rthalla@luc.edu>
2024-08-16 01:22:38 -0500

    Update tet2_ripk3_rnaseq.R

--- tet2_ripk3_rnaseq.R
+++ tet2_ripk3_rnaseq.R
@@ -3,7 +3,7 @@ library(tximport)
 library(tidyverse)
 library(rnaseqGene)
 library(pheatmap)
-setwd("C:/Users/rthalla/OneDrive - Loyola University Chicago/Zhang Lab/Kanak Tet2 Ripk3/")
+setwd("C:/Users/rohit/OneDrive - Loyola University Chicago/Zhang Lab/Kanak Tet2 Ripk3/")
 
 #DESeq2  analysis ------------------------
 #Load data into DESeq dataset
@@ -22,6 +22,7 @@ dds <- DESeq(dds)
 rescmp <- results(dds) 
 #write results as csv
 write.csv(rescmp, "completeresults.csv")
+#------------------------------------------------------------------------------------------
 
 # Comparison 1 of 3:  DKO-WT
 res1 <- DESeq2::results(dds, 
@@ -63,10 +64,10 @@ res_filtered3 <- res3[which(res3$padj < 0.1), ]
 write.csv(res_filtered3, "dko_tet2ko_filter.csv")
 
 write.csv(res3, "dko_tet2ko.csv")
+#-------------------------------------------------------------------------------------
 
 
-
-#display as heatmap
+#display top 20 as heatmap
 ntd <- normTransform(dds)
 select <- order(rowMeans(counts(dds,normalized=TRUE)),
                 decreasing=TRUE)[1:20]
@@ -81,3 +82,44 @@ pheatmap(assay(ntd)[select,], cluster_rows=FALSE, show_rownames=TRUE,
 # For saving the plot
 pheatmap(assay(ntd)[select,], cluster_rows=FALSE, show_rownames=TRUE,
          cluster_cols=FALSE, annotation_col=df, filename="heatmap.pdf")
+#------------------------------------------------------------------------------------------
+
+#Display selected Genes
+# Assuming 'dds' is your DESeqDataSet object and 'res' is your results object from DESeq2
+# Extract normalized counts
+norm_counts <- counts(dds, normalized=TRUE)
+
+# List of specific genes
+genes_of_interest <- c("Rgs1", "Sh2d1a", "Crip1", "S100a13", "Gabbr1", "Dtnb", "Rsad2", "Cxcl9", 
+                       "Kenj10", "Gstm5", "C1qb", "Slc40a1", "Slc43a1", "C1qc", "Veam1", "Marchf1", 
+                       "Pla2g2d", "C1qa", "Slc25a37", "Ckap4", "Rgl1", "Mafb", "Pou2af1", "Ighm", 
+                       "Il18", "Slco2b1", "Marcks", "Itga9", "Tns3", "Abcc3", "Wdfy3", "Arhgap23")
+
+# Define the desired column order
+desired_order <- c("WT_1", "WT_2", "WT_3", "Tet2KO_1", "Tet2KO_2", "Tet2KO_3", "DKO_1", "DKO_2", "DKO_3")
+
+# Reorder the columns
+subset_norm_counts <- subset_norm_counts[, desired_order]
+
+# Extract column data (sample information) and reorder it
+col_data <- colData(dds)[desired_order, ]
+
+# Define color palette
+color_palette <- colorRampPalette(c("blue", "white", "red"))(100)
+
+# Define annotation colors for the conditions
+annotation_colors <- list(
+  group = c(WT = "blue", Tet2KO = "orange", DKO = "purple")
+)
+
+# Create a heatmap
+pheatmap(subset_norm_counts, 
+         scale = "row", 
+         clustering_distance_rows = "euclidean", 
+         cluster_cols = FALSE, 
+         clustering_method = "complete", 
+         color = color_palette,
+         show_rownames = TRUE, 
+         show_colnames = TRUE,
+         main = "Heatmap of AITL Genes")
+
